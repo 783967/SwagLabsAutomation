a918b5b58bc1192c6b8d3ef4d6164f2f7a730e19
2280571 <subhamay.ghosh@cognizant.com>
2024-11-26 22:18:30 +0530

    Violated Google coding style

--- src/test/java/swaglabs/tests/validateHomePageTest.java
+++ src/test/java/swaglabs/tests/validateHomePageTest.java
@@ -1,8 +1,15 @@
 package swaglabs.tests;
 
 import org.testng.annotations.Test;
+
+import swaglabs.common.CommonMethods;
+import static swaglabs.common.CommonMethods.loginPageButton;
 import swaglabs.components.DriverFactory;
 import swaglabs.steps.HomePageSteps;
+import static swaglabs.common.CommonMethods.loginPageCancelButton;
+
+
+
 
 public class validateHomePageTest extends DriverFactory {
 
@@ -17,6 +24,13 @@ public class validateHomePageTest extends DriverFactory {
 		HomePageSteps homePageSteps = new HomePageSteps();
 		homePageSteps.loginToSwagLabs();
 		homePageSteps.addSauceLabsBackpackIntoCartAndVerify();
+	}
+	
+	@Test
+	public void VALIDATELoginPage(){
+		HomePageSteps homePageSteps = new HomePageSteps();
+		homePageSteps.loginToSwagLabs();
+		CommonMethods.loginPageCancelButton(1);
 	}
 	
 }
