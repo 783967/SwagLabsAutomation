7f2bc4d0ef7e2618127ac9a136c6cd80100aade8
Ghosh <2280571@cognizant.com>
2024-11-29 15:16:02 +0530

    added RestClient constructor

--- src/main/java/swaglabs/common/RestClient.java
+++ src/main/java/swaglabs/common/RestClient.java
@@ -15,97 +15,79 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import swaglabs.components.BasePage;
+import swaglabs.steps.HomePageSteps;
 
 public class RestClient {
 
-	private static Logger logger = BasePage.getLogger();
+	private static Logger logger;
+
+	public RestClient() {
+		BasePage.setLogger(HomePageSteps.class);
+		logger = BasePage.getLogger();
+	}
 
 	/**
 	 * Validate Get Call
+	 * 
 	 * @param url
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
 	public void get(String url, String serviceUrl) {
-		HttpHost proxy = new HttpHost("proxy.cognizant.com", 8080); 
-        RequestConfig config = RequestConfig.custom()
-                .setProxy(proxy)
-                .setConnectTimeout(10000)
-                .setSocketTimeout(10000)
-                .build();
+		HttpHost proxy = new HttpHost("proxy.cognizant.com", 8080);
+		RequestConfig config = RequestConfig.custom().setProxy(proxy).setConnectTimeout(10000).setSocketTimeout(10000)
+				.build();
 
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setDefaultRequestConfig(config)
-                .build();
+		CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(config).build();
 		// Base URI
-       RestAssured.baseURI = url;
-       
-        // GET request
-        Response response = RestAssured
-                .given()
-                .when()
-                .get(serviceUrl)
-                .then()
-                .statusCode(200)
-                .extract()
-                .response();
+		RestAssured.baseURI = url;
 
-        // Print status code
-        Assert.assertEquals(200, response.getStatusCode());
-        logger.info("Status Code -> " + response.getStatusCode());
+		// GET request
+		Response response = RestAssured.given().when().get(serviceUrl).then().statusCode(200).extract().response();
 
-        // Print response body
-        String responseBody = response.getBody().asString();
-        System.out.println("Response Body -> " + responseBody);
+		// Print status code
+		Assert.assertEquals(200, response.getStatusCode());
+		logger.info("Status Code -> " + response.getStatusCode());
 
-        // Convert response body to JSON
-        JSONObject responseJson = new JSONObject(responseBody);
-        System.out.println("Response JSON -> " + responseJson.toString(4));
+		// Print response body
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body -> " + responseBody);
+
+		// Convert response body to JSON
+		JSONObject responseJson = new JSONObject(responseBody);
+		System.out.println("Response JSON -> " + responseJson.toString(4));
 	}
-	
+
 	public void post(String url, String serviceUrl) {
 		// Proxy configuration
-        HttpHost proxy = new HttpHost("proxy.cognizant.com", 8080);
-        RequestConfig config = RequestConfig.custom()
-                .setProxy(proxy)
-                .setConnectTimeout(10000)
-                .setSocketTimeout(10000)
-                .build();
+		HttpHost proxy = new HttpHost("proxy.cognizant.com", 8080);
+		RequestConfig config = RequestConfig.custom().setProxy(proxy).setConnectTimeout(10000).setSocketTimeout(10000)
+				.build();
 
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setDefaultRequestConfig(config)
-                .build();
+		CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(config).build();
 
-        // Base URI
-        RestAssured.baseURI = url; // Replace with your URL
+		// Base URI
+		RestAssured.baseURI = url; // Replace with your URL
 
-        // Request body
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("name", "morpheus");
-        requestBody.put("job", "leader");
-        
-        Response response = RestAssured
-                .given()
-                .body(requestBody.toString())
-                .post(serviceUrl)
-                .then()
-                .statusCode(201)
-                .extract()
-                .response();
-   
-        // Print status code
-        Assert.assertEquals(201, response.getStatusCode());
-        logger.info("Status Code -> " + response.getStatusCode());
+		// Request body
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("name", "morpheus");
+		requestBody.put("job", "leader");
 
-        // Print response body
-        String responseBody = response.getBody().asString();
-        System.out.println("Response Body -> " + responseBody);
+		Response response = RestAssured.given().body(requestBody.toString()).post(serviceUrl).then().statusCode(201)
+				.extract().response();
 
-        // Convert response body to JSON
-        JSONObject responseJson = new JSONObject(responseBody);
-        System.out.println("Response JSON -> " + responseJson.toString(4));
+		// Print status code
+		Assert.assertEquals(201, response.getStatusCode());
+		logger.info("Status Code -> " + response.getStatusCode());
+
+		// Print response body
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body -> " + responseBody);
+
+		// Convert response body to JSON
+		JSONObject responseJson = new JSONObject(responseBody);
+		System.out.println("Response JSON -> " + responseJson.toString(4));
 	}
-	
-	
 
 }
