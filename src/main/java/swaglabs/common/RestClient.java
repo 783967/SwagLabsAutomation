ddcfdb9e4128eda51a64ef1e56816f22266e692f
Ghosh <2280571@cognizant.com>
2024-11-29 15:07:47 +0530

    updated api scripts

--- src/main/java/swaglabs/common/RestClient.java
+++ src/main/java/swaglabs/common/RestClient.java
@@ -1,21 +1,19 @@
 package swaglabs.common;
 
 import java.io.IOException;
-import java.util.HashMap;
-import org.apache.http.Header;
+
+import org.apache.http.HttpHost;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 import org.testng.Assert;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import swaglabs.components.BasePage;
 
 public class RestClient {
@@ -28,77 +26,86 @@ public class RestClient {
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
-	public void get(String url) throws ClientProtocolException, IOException {
-		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000) // Connection timeout
-				.setSocketTimeout(10000) // Socket timeout
-				.build();
-		CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig).build();
-		HttpGet httpGet = new HttpGet(url);
-		CloseableHttpResponse closableHttpResponse = httpClient.execute(httpGet);
+	public void get(String url, String serviceUrl) {
+		HttpHost proxy = new HttpHost("proxy.cognizant.com", 8080); 
+        RequestConfig config = RequestConfig.custom()
+                .setProxy(proxy)
+                .setConnectTimeout(10000)
+                .setSocketTimeout(10000)
+                .build();
 
-		// Get Status Code
-		int statusCode = closableHttpResponse.getStatusLine().getStatusCode();
-		logger.info("Status Code -> " + statusCode);
-		Assert.assertEquals(200, 200);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(config)
+                .build();
+		// Base URI
+       RestAssured.baseURI = url;
+       
+        // GET request
+        Response response = RestAssured
+                .given()
+                .when()
+                .get(serviceUrl)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
 
-		// Get response body
-				String responseString = EntityUtils.toString(closableHttpResponse.getEntity(), "UTF-8");
-		JSONObject responseJson = new JSONObject(responseString);
-		logger.info("Response JSON from API -> " + responseJson);
+        // Print status code
+        Assert.assertEquals(200, response.getStatusCode());
+        logger.info("Status Code -> " + response.getStatusCode());
 
-		// Get headers
-		Header[] headerArray = closableHttpResponse.getAllHeaders();
-		HashMap<String, String> allHeaders = new HashMap<String, String>();
+        // Print response body
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body -> " + responseBody);
 
-		for (Header header : headerArray) {
-			allHeaders.put(header.getName(), header.getValue());
-		}
-
-		System.out.println("Headers Array -> " + allHeaders);
+        // Convert response body to JSON
+        JSONObject responseJson = new JSONObject(responseBody);
+        System.out.println("Response JSON -> " + responseJson.toString(4));
 	}
 	
-	/**
-	 * Validate Post Call
-	 * @param url
-	 * @param jsonPayload
-	 * @throws ClientProtocolException
-	 * @throws IOException
-	 */
-	public void post(String url, JSONObject jsonPayload) throws ClientProtocolException, IOException {
-	    RequestConfig requestConfig = RequestConfig.custom()
-	            .setConnectTimeout(10000) // Connection timeout
-	            .setSocketTimeout(10000) // Socket timeout
-	            .build();
-	    CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig).build();
-	    HttpPost httpPost = new HttpPost(url);
+	public void post(String url, String serviceUrl) {
+		// Proxy configuration
+        HttpHost proxy = new HttpHost("proxy.cognizant.com", 8080);
+        RequestConfig config = RequestConfig.custom()
+                .setProxy(proxy)
+                .setConnectTimeout(10000)
+                .setSocketTimeout(10000)
+                .build();
 
-	    // Set the payload
-	    StringEntity entity = new StringEntity(jsonPayload.toString());
-	    httpPost.setEntity(entity);
-	    httpPost.setHeader("Content-type", "application/json");
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(config)
+                .build();
 
-	    CloseableHttpResponse closableHttpResponse = httpClient.execute(httpPost);
+        // Base URI
+        RestAssured.baseURI = url; // Replace with your URL
 
-	    // Get Status Code
-	    int statusCode = closableHttpResponse.getStatusLine().getStatusCode();
-	    logger.info("Status Code -> " + statusCode);
-	    Assert.assertEquals(200, statusCode);
+        // Request body
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "morpheus");
+        requestBody.put("job", "leader");
+        
+        Response response = RestAssured
+                .given()
+                .body(requestBody.toString())
+                .post(serviceUrl)
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+   
+        // Print status code
+        Assert.assertEquals(201, response.getStatusCode());
+        logger.info("Status Code -> " + response.getStatusCode());
 
-	    // Get response body
-	    String responseString = EntityUtils.toString(closableHttpResponse.getEntity(), "UTF-8");
-	    JSONObject responseJson = new JSONObject(responseString);
-	    logger.info("Response JSON from API -> " + responseJson);
+        // Print response body
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body -> " + responseBody);
 
-	    // Get headers
-	    Header[] headerArray = closableHttpResponse.getAllHeaders();
-	    HashMap<String, String> allHeaders = new HashMap<String, String>();
-
-	    for (Header header : headerArray) {
-	        allHeaders.put(header.getName(), header.getValue());
-	    }
-
-	    System.out.println("Headers Array -> " + allHeaders);
+        // Convert response body to JSON
+        JSONObject responseJson = new JSONObject(responseBody);
+        System.out.println("Response JSON -> " + responseJson.toString(4));
 	}
-
+	
+	
 
 }
--- src/test/java/swaglabs/tests/validateApiScript.java
+++ src/test/java/swaglabs/tests/validateApiScript.java
@@ -1,9 +1,5 @@
 package swaglabs.tests;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import swaglabs.common.CommonMethods;
@@ -12,20 +8,16 @@ import swaglabs.common.RestClient;
 public class validateApiScript {
 
 	@Test(priority = 1, description = "Validate Get Call")
-	public void validateGetCall() throws ClientProtocolException, IOException {
+	public void validateGetCall() {
 		RestClient restClient = new RestClient();
-		String apiURL = CommonMethods.getPropertyValue("apiURL") + CommonMethods.getPropertyValue("serviceURL");
-		restClient.get(apiURL);
+		restClient.get(CommonMethods.getPropertyValue("apiURL"), CommonMethods.getPropertyValue("serviceURL"));
+	}
+	
+	@Test
+	public void validatePostCall() {
+		RestClient restClient = new RestClient();
+		restClient.post(CommonMethods.getPropertyValue("apiURL"), CommonMethods.getPropertyValue("serviceURL"));
 	}
 
-	@Test(priority = 2, description = "Validate Post Call")
-	public void validatePostCall() throws ClientProtocolException, IOException {
-		RestClient restClient = new RestClient();
-		String apiURL = CommonMethods.getPropertyValue("apiURL") + CommonMethods.getPropertyValue("serviceURL");
-		JSONObject jsonPayload = new JSONObject();
-		jsonPayload.put("title", "foo");
-		jsonPayload.put("body", "bar");
-		jsonPayload.put("userId", 1);
-		restClient.post(apiURL, jsonPayload);
-	}
+	
 }
