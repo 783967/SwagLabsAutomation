30163c929b8930d2c5ee6ae883220a022d260e64
2280571 <2280571@cognizant.com>
2024-12-06 15:49:09 +0530

    ok

--- src/main/java/swaglabs/common/CommonMethods.java
+++ src/main/java/swaglabs/common/CommonMethods.java
@@ -129,4 +129,5 @@ public class CommonMethods extends BasePage {
 	public void TestTheSwagLabsByLoginIntoItAndThenValidateUserIsSuccessfullyLandedIntoItAfterThatLogoutFromAppilication() {
 		
 	}
+	
 }
